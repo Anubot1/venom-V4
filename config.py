e11d74d3e87dddd1fa042106f8761683cf691a35
Anubot1 <auradeptstore1000@gmail.com>
2024-07-24 18:18:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7240966462:AAFyoFxDwi3qciu6x_Ha4D9hUQXqjFSDzQA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Botanuvenom")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7289643911")
 
 
 
